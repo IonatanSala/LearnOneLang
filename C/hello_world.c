3514cd742c7a23a9ed5367fdb76705476c8c4a7c
Ionatan Sala <sala.jonatan1995@gmail.com>
2015-09-21 22:28:33 +0100

    added what c is to intro.c and a basic hello world program

---
+++ C/hello_world.c
@@ -0,0 +1,26 @@
+// A basic C program consits of the following:
+	// Preprocessor Commands
+	// Functions
+	// Variables
+	// Statements & Expressions
+	// Comments
+
+#include <stdio.h>
+
+int main() {
+
+	printf("Hello, World! \n");
+
+	return 0;
+}
+
+// What does the aboe code do?
+
+	// The first lin (#include <stdio.h>) is a preprocessor command, this tells a C compiler
+	// to include stdio.h file before going to acula compilation.
+
+	// int main(), this is where the program execution begins
+
+	// printf(...), is a function in C that causes the message inside the function to display on screen
+
+	// return 0; , this terminates the main() function and returns the value 0;
---
+++ C/intro.c
@@ -0,0 +1,26 @@
+// C is a general-purpose, imperative programming language.
+// Developed by Dennis Ritchie between 1969 - 1973 at AT&T Bell Labs and was used to
+// implement the Unix operating system.
+// Many languages borrowed directyly and indirectly from C, including:
+	// C++, D, Go, Rust, Java, JavaScript, Limbo, LPC, C#, Objective-C, Perl, PHP, Python, Verilog
+
+// C structured language
+// C is useful for many application that had formerly been coded in assembly language.
+// C produces efficient programs
+// C can handle low-lecel activities
+// Today C is the most popular System Programming Language.
+// C was also designed to encourage cross-platform programming.
+// C can be comipled for a very wide variety of computer platforms and operating systems with few changes to its source code.
+
+// C was initially used for system development work, in particular the programs that make-up the operating system
+// C was adopted as a system development language because it produces code that runs nearly as fast as code written in assembly language
+// C is used in: 
+// Operating Systems, Language Compilers, Assemblers, Text Editors, Print Spoolers, Network Drivers, Modern Programs, Databases, Language Interpreters, Utilities.
+
+
+
+
+
+
+
+
